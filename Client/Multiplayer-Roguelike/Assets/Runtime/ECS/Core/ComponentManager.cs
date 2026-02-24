e04c33f9d2751f84bd44efeeb5fa77f37a7454fe
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-02-24 20:56:07 +0300

    chg: refactor ComponentManager.cs

--- Client/Multiplayer-Roguelike/Assets/Runtime/ECS/Core/ComponentManager.cs
+++ Client/Multiplayer-Roguelike/Assets/Runtime/ECS/Core/ComponentManager.cs
@@ -7,35 +7,35 @@ namespace Runtime.ECS.Core
 {
     public class ComponentManager
     {
-        private readonly Dictionary<Type, IComponentStorage<IComponent>> _storage = new Dictionary<Type, IComponentStorage<IComponent>>();
+        private readonly Dictionary<Type, IComponentStorage<IComponent>> _storage = new();
 
         public void AddComponent<T>(int entityId, T component) where T : class, IComponent
         {
             GetStorage<T>().Add(entityId, component);
         }
-        
+
         public IEnumerable<int> GetAllEntities()
         {
             return _storage.Values.SelectMany(s => s.EntityIds).Distinct();
         }
-        
+
         public IEnumerable<Type> GetComponentTypes(int entityId)
         {
-            return from kvp in _storage where kvp.Value.Has(entityId) select kvp.Key;
+            return from pair in _storage where pair.Value.Has(entityId) select pair.Key;
         }
-        
+
         public object GetComponent(int entityId, Type componentType)
         {
             if (!_storage.TryGetValue(componentType, out var storage))
             {
                 return null;
             }
-            
+
             storage.TryGet(entityId, out var component);
             return component;
         }
 
-        public IEnumerable<(int entityId, object[] components)> Query(params Type[] componentTypes) 
+        public IEnumerable<(int entityId, object[] components)> Query(params Type[] componentTypes)
         {
             var storages = componentTypes.Select(t => _storage[t]).ToArray();
 
@@ -48,21 +48,20 @@ namespace Runtime.ECS.Core
 
             foreach (var id in entityIds)
             {
-                var components = storages
-                    .Select(s => s.TryGet(id, out var component) ? component : null)
-                    .ToArray();
-                
+                var components = storages.Select(s => s.TryGet(id, out var component) ? component : null).ToArray();
+
                 yield return (id, components);
             }
         }
+
         private ComponentStorage<T> GetStorage<T>() where T : class, IComponent
         {
             if (!_storage.TryGetValue(typeof(T), out var storage))
             {
                 _storage[typeof(T)] = new ComponentStorage<T>();
             }
-            
-            return (ComponentStorage<T>) _storage[typeof(T)];
+
+            return (ComponentStorage<T>)_storage[typeof(T)];
         }
     }
 }
