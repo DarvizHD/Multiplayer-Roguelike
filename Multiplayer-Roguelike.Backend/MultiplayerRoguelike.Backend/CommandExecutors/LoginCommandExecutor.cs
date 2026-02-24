3b5a36343b0ba88be9122c18257c83ab74a70c5c
DmitryDemenkov <demon.krut2002@mail.ru>
2026-02-25 00:36:32 +0300

    chg: login logic

--- Multiplayer-Roguelike.Backend/MultiplayerRoguelike.Backend/CommandExecutors/LoginCommandExecutor.cs
+++ Multiplayer-Roguelike.Backend/MultiplayerRoguelike.Backend/CommandExecutors/LoginCommandExecutor.cs
@@ -1,4 +1,5 @@
 using System;
+using Backend.Player;
 using ENet;
 using Shared.Commands;
 
@@ -14,6 +15,9 @@ namespace Backend.CommandExecutors
         public override void Execute()
         {
             Console.WriteLine($"Player {Command.PlayerNickname} wants to login");
+
+            var player = new PlayerModel(Command.PlayerNickname, Peer);
+            World.Players.Add(player.PlayerNickname, player);
         }
     }
 }
