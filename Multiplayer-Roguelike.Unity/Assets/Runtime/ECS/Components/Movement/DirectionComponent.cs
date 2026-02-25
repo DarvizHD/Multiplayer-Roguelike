4e08e45b2e1f19dce94fd059758d4713c6cbd553
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-02-25 03:10:30 +0300

    add: new properties in direction component

--- Multiplayer-Roguelike.Unity/Assets/Runtime/ECS/Components/Movement/DirectionComponent.cs
+++ Multiplayer-Roguelike.Unity/Assets/Runtime/ECS/Components/Movement/DirectionComponent.cs
@@ -5,6 +5,8 @@ namespace Runtime.ECS.Components.Movement
     public class DirectionComponent : IComponent
     {
         public Vector3 Direction;
+        public Vector3 AvoidanceVelocity;
+        public Vector3 DesiredDirection;
         
         public DirectionComponent(Vector3 direction)
         {
