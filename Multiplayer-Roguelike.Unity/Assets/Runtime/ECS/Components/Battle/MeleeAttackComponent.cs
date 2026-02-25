debae29b9a4d9d774b0d16625ec0cb79e0acd003
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-02-25 23:17:15 +0300

    add: melee attack component

--- Multiplayer-Roguelike.Unity/Assets/Runtime/ECS/Components/Battle/MeleeAttackComponent.cs
+++ Multiplayer-Roguelike.Unity/Assets/Runtime/ECS/Components/Battle/MeleeAttackComponent.cs
@@ -2,14 +2,15 @@ namespace Runtime.ECS.Components.Battle
 {
     public class MeleeAttackComponent : IComponent
     {
-        public float Range;
+        public float Range {get; set;}
+        public float Angle { get; set; }
+        public float Damage {get; set;} 
 
-        public float Damage;
-
-        public MeleeAttackComponent(float range, float damage)
+        public MeleeAttackComponent(float range, float damage, float angle = 90f)
         {
             Range = range;
             Damage = damage;
+            Angle = angle;
         }
     }
 }
