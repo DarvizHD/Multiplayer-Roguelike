7f4d73b925edaacc0dc7b0a9750563dde186d814
Maxim S. <its3zin.main@gmail.com>
2026-02-25 21:53:41 +0300

    add: animation by local move position

--- Multiplayer-Roguelike.Unity/Assets/Runtime/ECS/Systems/Movement/DrawTransformSystem.cs
+++ Multiplayer-Roguelike.Unity/Assets/Runtime/ECS/Systems/Movement/DrawTransformSystem.cs
@@ -1,4 +1,5 @@
 using Runtime.ECS.Components.Movement;
+using UnityEngine;
 
 namespace Runtime.ECS.Systems.Movement
 {
@@ -10,15 +11,15 @@ namespace Runtime.ECS.Systems.Movement
             RegisterRequiredComponent(typeof(RotationComponent));
             RegisterRequiredComponent(typeof(TransformComponent));
         }
-
+        
         protected override void Update(int id, object[] components, float deltaTime)
         {
-            var positionComponent = components[0] as PositionComponent;
+            var positionComponent =  components[0] as PositionComponent;
             var rotationComponent = components[1] as RotationComponent;
             var transformComponent = components[2] as TransformComponent;
-
-            transformComponent!.Transform.position = positionComponent!.Position;
-            transformComponent.Transform.rotation = rotationComponent!.Rotation;
+            
+            transformComponent.Transform.position = positionComponent.Position;
+            transformComponent.Transform.rotation = Quaternion.Euler(0f, rotationComponent.Angle, 0f);
         }
     }
 }
