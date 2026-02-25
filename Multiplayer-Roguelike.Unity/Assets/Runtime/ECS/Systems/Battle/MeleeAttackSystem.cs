ecd83c8a0aae4623f9e99cf1d5163b45cbd917c9
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-02-25 23:28:28 +0300

    chg: refactor MeleeAttackSystem.cs

--- Multiplayer-Roguelike.Unity/Assets/Runtime/ECS/Systems/Battle/MeleeAttackSystem.cs
+++ Multiplayer-Roguelike.Unity/Assets/Runtime/ECS/Systems/Battle/MeleeAttackSystem.cs
@@ -1,3 +1,4 @@
+using Runtime.ECS.Components;
 using Runtime.ECS.Components.Battle;
 using Runtime.ECS.Components.Movement;
 using UnityEngine;
@@ -9,6 +10,7 @@ namespace Runtime.ECS.Systems.Battle
         public MeleeAttackSystem()
         {
             RegisterRequiredComponent(typeof(PositionComponent));
+            RegisterRequiredComponent(typeof(DirectionComponent));
             RegisterRequiredComponent(typeof(MeleeAttackComponent));
             RegisterRequiredComponent(typeof(AttackCooldownComponent));
         }
@@ -16,8 +18,9 @@ namespace Runtime.ECS.Systems.Battle
         protected override void Update(int id, object[] components, float deltaTime)
         {
             var positionComponent = components[0] as PositionComponent;
-            var meleeAttackComponent = components[1] as MeleeAttackComponent;
-            var attackCooldownComponent = components[2] as AttackCooldownComponent;
+            var directionComponent = components[1] as DirectionComponent;
+            var meleeAttackComponent = components[2] as MeleeAttackComponent;
+            var attackCooldownComponent = components[3] as AttackCooldownComponent;
 
             if (attackCooldownComponent.CurrentCooldown > 0)
             {
@@ -25,6 +28,9 @@ namespace Runtime.ECS.Systems.Battle
             }
 
             attackCooldownComponent.CurrentCooldown = attackCooldownComponent.Cooldown;
+
+            var attackDirection = directionComponent.Direction;
+            attackDirection.y = 0;
 
             var targets = ComponentManager.Query(typeof(PositionComponent), typeof(EnemyTagComponent));
 
@@ -34,14 +40,31 @@ namespace Runtime.ECS.Systems.Battle
 
                 var distance = Vector3.Distance(targetPositionComponent.Position, positionComponent.Position);
 
-                if (distance < meleeAttackComponent.Range)
+                if (distance >= meleeAttackComponent.Range)
                 {
-                    if (!ComponentManager.TryGetComponent<PendingDamageEventComponent>(targetId, out var pendingDamageEventComponent))
-                    {
-                        ComponentManager.AddComponent(targetId, pendingDamageEventComponent = new PendingDamageEventComponent());
-                    }
+                    continue;
+                }
 
-                    pendingDamageEventComponent.TotalDamage += meleeAttackComponent.Damage;
+                var toTarget = (targetPositionComponent.Position - positionComponent.Position);
+                toTarget.y = 0;
+                
+                toTarget.Normalize();
+
+                var angle = Vector3.Angle(attackDirection, toTarget);
+
+                Debug.Log($"Before Attack {id} -> {targetId} {meleeAttackComponent.Damage}");
+                
+                if (angle > meleeAttackComponent.Angle * 0.5f)
+                {
+                    continue;
+                }
+
+                Debug.Log($"Attack {id} -> {targetId} {meleeAttackComponent.Damage}");
+                
+                if (!ComponentManager.TryGetComponent<AttackEventComponent>(id, out var attackEventComponent))
+                {
+                    Debug.Log($"Add Attack {id} -> {targetId} {meleeAttackComponent.Damage}");
+                    ComponentManager.AddComponent(id, new AttackEventComponent(targetId, meleeAttackComponent.Damage));
                 }
             }
         }
