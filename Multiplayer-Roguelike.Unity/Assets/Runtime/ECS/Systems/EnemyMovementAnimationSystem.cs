96f3f85828a54e10f81994ead3c816a90c456953
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-02-26 13:51:40 +0300

    chg: namespaces in EnemyMovementAnimationSystem.cs

--- Multiplayer-Roguelike.Unity/Assets/Runtime/ECS/Systems/EnemyMovementAnimationSystem.cs
+++ Multiplayer-Roguelike.Unity/Assets/Runtime/ECS/Systems/EnemyMovementAnimationSystem.cs
@@ -1,6 +1,6 @@
 using Runtime.ECS.Components;
-using Runtime.ECS.Components.Battle;
 using Runtime.ECS.Components.Movement;
+using Runtime.ECS.Components.Tags;
 using UnityEngine;
 
 namespace Runtime.ECS.Systems
@@ -13,13 +13,13 @@ namespace Runtime.ECS.Systems
             RegisterRequiredComponent(typeof(AnimatorComponent));
             RegisterRequiredComponent(typeof(DirectionComponent));
         }
-        
+
         protected override void Update(int id, object[] components, float deltaTime)
         {
             var enemyTagComponent = components[0] as EnemyTagComponent;
             var animatorComponent = components[1] as AnimatorComponent;
             var directionComponent = components[2] as DirectionComponent;
-            
+
             animatorComponent.Animator.SetBool(animatorComponent.IsRun, directionComponent.Direction != Vector3.zero);
         }
     }
