0fc11c3a8f5ff966980cdce7d21ca7209a459510
DmitryDemenkov <demon.krut2002@mail.ru>
2026-03-02 17:13:42 +0300

    chg: server test script

--- Multiplayer-Roguelike.Unity/Assets/Runtime/TestServer/TestServerScript.cs
+++ Multiplayer-Roguelike.Unity/Assets/Runtime/TestServer/TestServerScript.cs
@@ -7,22 +7,25 @@ using Shared.Commands;
 using Shared.Protocol;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 using Vector3 = Shared.Primitives.Vector3;
 
 public class TestServerScript : MonoBehaviour
 {
-    [SerializeField] private TestCharacterModel _character;
+    [SerializeField] private TestCharacterModel _characterPrefab;
 
     private readonly GameSystemCollection _gameFixedSystemCollection = new();
     private ServerConnectionModel _serverConnectionModel;
 
     private TestPlayerModel _player;
+    private TestWorldModel _world;
 
     private async void Start()
     {
         _serverConnectionModel = new ServerConnectionModel();
-        var serverConnectionPresenter = new ServerConnectionPresenter(_serverConnectionModel, _gameFixedSystemCollection);
+        var serverConnectionPresenter =
+            new ServerConnectionPresenter(_serverConnectionModel, _gameFixedSystemCollection);
         serverConnectionPresenter.Enable();
 
         _serverConnectionModel.ConnectPlayer();
@@ -32,12 +35,13 @@ public class TestServerScript : MonoBehaviour
         _playerControl.Enable();
 
         _player = new TestPlayerModel();
-        _character.Model = _player.PlayerSharedModel.Character;
+        _world = new TestWorldModel();
 
-        _serverConnectionModel.PacketReceived += OnPacketReceived;
+        _serverConnectionModel.PlayerPacketReceived += OnPlayerPacketReceived;
+        _serverConnectionModel.WorldPacketReceived += OnWorldPacketReceived;
     }
 
-    private void OnPacketReceived(Packet packet)
+    private void OnPlayerPacketReceived(Packet packet)
     {
         var buffer = new byte[1024];
         packet.CopyTo(buffer);
@@ -45,17 +49,33 @@ public class TestServerScript : MonoBehaviour
         var protocol = new NetworkProtocol(buffer);
         protocol.Get(out string id);
         _player.PlayerSharedModel.Read(protocol);
+
+        lobbyID = _player.PlayerSharedModel.Lobby.LobbyId.Value;
     }
 
-    private void OnMoved(Vector2 moveInput)
+    private void OnWorldPacketReceived(Packet packet)
     {
-        var direction = new Vector3(moveInput.x, 0, moveInput.y);
+        var buffer = new byte[1024];
+        packet.CopyTo(buffer);
 
-        var moveCommand = new MoveCommand(_nicknameInputField.text, direction);
-        moveCommand.Write(_serverConnectionModel.PlayerPeer);
+        var protocol = new NetworkProtocol(buffer);
+        protocol.Get(out string id);
+        _world.World.Read(protocol);
+
+        foreach (var characterSharedModel in _world.World.Characters.Models)
+        {
+            if (!_world.Characters.ContainsKey(characterSharedModel.Id))
+            {
+                var newCharacter = Instantiate(_characterPrefab, UnityEngine.Vector3.zero, Quaternion.identity);
+                newCharacter.Model = characterSharedModel;
+                newCharacter.NameText.text = characterSharedModel.Id;
+                _world.Characters.Add(characterSharedModel.Id, newCharacter);
+            }
+        }
     }
 
     private Vector2 _lastInput = Vector2.zero;
+    private PlayerControls _playerControl;
 
     private void Update()
     {
@@ -74,25 +94,99 @@ public class TestServerScript : MonoBehaviour
         _gameFixedSystemCollection.Update(Time.fixedDeltaTime);
     }
 
-    [SerializeField] private InputField _nicknameInputField;
-    [SerializeField] private InputField _lobbyIdInputField;
-    private PlayerControls _playerControl;
+    private Rect windowRect;
+    private int windowWidth = 400;
+    private int windowHeight = 220;
+
+    private string nickname = "";
+    private string lobbyID = "";
+
+    private void OnGUI()
+    {
+        windowRect = new Rect(
+            Screen.width - windowWidth - 15,
+            15,
+            windowWidth,
+            windowHeight
+        );
+
+        windowRect = GUI.Window(0, windowRect, DrawWindow, "");
+    }
+
+    private void DrawWindow(int id)
+    {
+        GUILayout.Space(15);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Nickname:", GUILayout.Width(80));
+        nickname = GUILayout.TextField(nickname, GUILayout.Width(250));
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(10);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Lobby ID:", GUILayout.Width(80));
+        lobbyID = GUILayout.TextField(lobbyID, GUILayout.Width(250));
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(20);
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Enter", GUILayout.Height(35)))
+        {
+            ConnectPlayer();
+        }
+
+        if (GUILayout.Button("Create Lobby", GUILayout.Height(35)))
+        {
+            CreateLobby();
+        }
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(10);
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Start", GUILayout.Height(35)))
+        {
+            StartSession();
+        }
+
+        if (GUILayout.Button("Join Lobby", GUILayout.Height(35)))
+        {
+            JoinLobby();
+        }
+        GUILayout.EndHorizontal();
+    }
 
     public void ConnectPlayer()
     {
-        var loginCommand = new LoginCommand(_nicknameInputField.text);
+        var loginCommand = new LoginCommand(nickname);
         loginCommand.Write(_serverConnectionModel.PlayerPeer);
     }
 
     public void CreateLobby()
     {
-        var createLobbyCommand = new CreateLobbyCommand(_nicknameInputField.text);
+        var createLobbyCommand = new CreateLobbyCommand(nickname);
         createLobbyCommand.Write(_serverConnectionModel.PlayerPeer);
     }
 
     public void JoinLobby()
     {
-        var joinLobbyCommand = new JoinLobbyCommand(_lobbyIdInputField.text, _nicknameInputField.text);
+        var joinLobbyCommand = new JoinLobbyCommand(lobbyID, nickname);
         joinLobbyCommand.Write(_serverConnectionModel.PlayerPeer);
+    }
+
+    public void StartSession()
+    {
+        var startSessionCommand = new StartSessionCommand(nickname, lobbyID);
+        startSessionCommand.Write(_serverConnectionModel.PlayerPeer);
+    }
+
+    private void OnMoved(Vector2 moveInput)
+    {
+        var direction = new Vector3(moveInput.x, 0, moveInput.y);
+
+        var moveCommand = new MoveCommand(nickname, direction);
+        moveCommand.Write(_serverConnectionModel.PlayerPeer);
     }
 }
