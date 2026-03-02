c6d92e6e7d2fa74b34fd2a2a9e00d7fc1bd405f3
Maxim S. <its3zin.main@gmail.com>
2026-03-03 01:38:16 +0300

    add: rotate command

---
+++ Multiplayer-Roguelike.Backend/MultiplayerRoguelike.Backend/CommandExecutors/RotateCommandExecutor.cs
@@ -0,0 +1,31 @@
+using System;
+using ENet;
+
+namespace Backend.CommandExecutors
+{
+    public class RotateCommandExecutor : BaseCommandExecutor<RotateCommand>
+    {
+        public RotateCommandExecutor(RotateCommand command, WorldModel world, Peer peer) : base(command, world, ref peer)
+        {
+
+        }
+
+        public override void Execute()
+        {
+            if (!World.Players.TryGet(Command.PlayerNickname, out var player))
+            {
+                Console.WriteLine($"Player {Command.PlayerNickname} not found");
+                return;
+            }
+
+            if (!World.Sessions.TryGet(player.SessionId, out var session))
+            {
+                Console.WriteLine($"Player {Command.PlayerNickname} has no session");
+                return;
+            }
+
+            session.WorldSharedModel.Characters.TryGet(player.PlayerSharedModel.Id, out var character);
+            character.Rotation.Value = Command.Rotation;
+        }
+    }
+}
