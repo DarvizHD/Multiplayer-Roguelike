5d8a9aa0e6d39e4290f8e4f6708a52f3b7929d20
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-03-04 17:56:50 +0300

    add: login screen presenter

---
+++ Multiplayer-Roguelike.Unity/Assets/Runtime/UI/Navigation/Screens/LoginScreenPresenter.cs
@@ -0,0 +1,37 @@
+using Runtime.UI.Menu;
+using Runtime.UI.Menu.Runtime.UI.Navigation;
+using Runtime.UI.Panels.LoginPanel;
+
+namespace Runtime.UI.Navigation.Screens
+{
+    public class LoginScreenPresenter : IPresenter
+    {
+        private readonly Router _router;
+        private readonly LoginPanelPresenter _presenter;
+        private readonly LoginPanelModel _model;
+
+        public LoginScreenPresenter(Router router, LoginPanelPresenter presenter, LoginPanelModel model)
+        {
+            _router = router;
+            _presenter = presenter;
+            _model = model;
+        }
+
+        public void Enable()
+        {
+            _model.OnConfirmed += HandleConfirmed;
+            _presenter.Enable();
+        }
+
+        public void Disable()
+        {
+            _model.OnConfirmed -= HandleConfirmed;
+            _presenter.Disable();
+        }
+
+        private void HandleConfirmed()
+        {
+            _router.NavigateTo(ScreenIds.StartMenu);
+        }
+    }
+}
