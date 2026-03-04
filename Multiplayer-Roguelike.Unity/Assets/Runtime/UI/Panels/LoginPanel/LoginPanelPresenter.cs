0609f5bb28e69b2d78ef59e842a5499ee1b4297b
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-03-04 17:53:55 +0300

    add: login panel

---
+++ Multiplayer-Roguelike.Unity/Assets/Runtime/UI/Panels/LoginPanel/LoginPanelPresenter.cs
@@ -0,0 +1,53 @@
+using Shared.Commands;
+using UnityEngine;
+
+namespace Runtime.UI.Panels.LoginPanel
+{
+    public class LoginPanelPresenter : IPresenter
+    {
+        private readonly LoginPanelView _view;
+        private readonly LoginPanelModel _model;
+        private readonly World _world;
+
+        public LoginPanelPresenter(LoginPanelModel model, LoginPanelView view, World world)
+        {
+            _model = model;
+            _view = view;
+            _world = world;
+        }
+
+        public void Enable()
+        {
+            _view.ParentRoot.Add(_view.Root);
+            _view.ConfirmButton.clicked += OnConfirmButtonClicked;
+
+            _world.PlayerSharedModel.Nickname.OnChange += OnNicknameChanged;
+        }
+
+        private void OnNicknameChanged()
+        {
+            _model.SetUsername(_world.PlayerSharedModel.Nickname.Value);
+
+            Debug.Log($"Nickname changed to {_model.Username}");
+
+            _model.Confirm();
+        }
+
+        public void Disable()
+        {
+            _view.ConfirmButton.clicked -= OnConfirmButtonClicked;
+            _view.ParentRoot.Remove(_view.Root);
+        }
+
+        private void OnConfirmButtonClicked()
+        {
+            if (string.IsNullOrEmpty(_view.UsernameTextField.value))
+            {
+                return;
+            }
+
+            var loginCommand = new LoginCommand(_view.UsernameTextField.value);
+            loginCommand.Write(_world.ServerConnectionModel.PlayerPeer);
+        }
+    }
+}
