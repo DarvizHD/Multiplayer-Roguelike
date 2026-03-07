47249794f386e965dbcbe572616e9bfeade12a71
Sergei Lind <sergei.lind@tutamail.com>
2026-03-07 15:18:38 +0300

    add: enemy model

---
+++ Multiplayer-Roguelike.Backend/MultiplayerRoguelike.Backend/Enemies/EnemyModel.cs
@@ -0,0 +1,21 @@
+﻿using Backend.Navigation;
+using Shared.Primitives;
+
+namespace Backend.Enemies
+{
+    public class EnemyModel
+    {
+        public int Id { get; }
+
+        public Vector3 Position;
+
+        public string TargetPlayerId;
+
+        public NavAgentModel Agent;
+
+        public EnemyModel(int id)
+        {
+            Id = id;
+        }
+    }
+}
