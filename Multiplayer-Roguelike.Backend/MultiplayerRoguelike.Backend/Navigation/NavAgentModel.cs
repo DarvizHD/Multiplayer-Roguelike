93777ec6175777bab3a66b8fa90e3b8afeead714
Sergei Lind <sergei.lind@tutamail.com>
2026-03-07 15:19:12 +0300

    add: navigation models

---
+++ Multiplayer-Roguelike.Backend/MultiplayerRoguelike.Backend/Navigation/NavAgentModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using DotRecast.Detour;
+using Shared.Primitives;
+
+namespace Backend.Navigation
+{
+    public class NavAgentModel
+    {
+        public const int MaxPath = 256;
+
+        public int Id { get; private set; }
+
+        public Vector3 Position { get; private set; }
+        public Vector3 Target { get; private set; }
+
+        public float Speed { get; private set; } = 2.5f;
+
+        public List<Vector3> Path { get; } = new();
+
+        public long[] PolyPath { get; } = new long[MaxPath];
+        public int PolyPathCount { get; set; }
+
+        public DtStraightPath[] StraightPath { get; } = new DtStraightPath[MaxPath];
+        public int StraightPathCount { get; set; }
+
+        public bool HasPath => Path.Count > 0;
+
+        public NavAgentModel(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Multiplayer-Roguelike.Backend/MultiplayerRoguelike.Backend/Navigation/NavigationWorldModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Backend.Navigation
+{
+    public class NavigationWorldModel
+    {
+        public Dictionary<int, NavAgentModel> Agents { get; } = new();
+    }
+}
