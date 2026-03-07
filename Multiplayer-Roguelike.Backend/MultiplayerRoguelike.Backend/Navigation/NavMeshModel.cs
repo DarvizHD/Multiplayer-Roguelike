eb8b274bc42ed55ffc7f5fa1af204049dc393cbd
Sergei Lind <sergei.lind@tutamail.com>
2026-03-07 15:19:12 +0300

    add: navigation models

---
+++ Multiplayer-Roguelike.Backend/MultiplayerRoguelike.Backend/Navigation/NavMeshModel.cs
@@ -0,0 +1,18 @@
+﻿using DotRecast.Detour;
+
+namespace Backend.Navigation
+{
+    public class NavMeshModel
+    {
+        public DtNavMesh NavMesh { get; set; }
+        public DtNavMeshQuery Query { get; set; }
+
+        public bool IsLoaded => NavMesh != null;
+
+        public void SetNavMesh(DtNavMesh navMesh)
+        {
+            NavMesh = navMesh;
+            Query = new DtNavMeshQuery(NavMesh);
+        }
+    }
+}
