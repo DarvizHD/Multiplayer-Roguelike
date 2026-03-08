9d665baba5b45475a96526e6c136a78053a36de2
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-03-08 18:47:34 +0300

    chg: namespaces in Movement folder

--- Multiplayer-Roguelike.Unity/Assets/Runtime/Ecs/Systems/Movement/PlayerMovementSystem.cs
+++ Multiplayer-Roguelike.Unity/Assets/Runtime/Ecs/Systems/Movement/PlayerMovementSystem.cs
@@ -1,10 +1,10 @@
+using Runtime.Ecs.Components;
 using Runtime.Ecs.Components.Health;
 using Runtime.Ecs.Components.Movement;
 using Runtime.Ecs.Components.Network;
 using Runtime.Ecs.Core;
-using Runtime.Ecs.Systems.Movement;
 
-namespace Runtime.ECS.Systems.Movement
+namespace Runtime.Ecs.Systems.Movement
 {
     public class PlayerMovementSystem : BaseSystem
     {
