9ab7ceb040c2ca393db2f676abf1ffe836ad8105
Sergei Lind <sergei.lind@tutamail.com>
2026-03-08 18:56:36 +0300

    add: test components for visual synchronisation of zombie positions

---
+++ Multiplayer-Roguelike.Unity/Assets/Runtime/Ecs/Components/Network/EnemyNetworkSyncComponent.cs
@@ -0,0 +1,14 @@
+using Shared.Models;
+
+namespace Runtime.Ecs.Components.Network
+{
+    public class EnemyNetworkSyncComponent : IComponent
+    {
+        public readonly EnemySharedModel EnemySharedModel;
+
+        public EnemyNetworkSyncComponent(EnemySharedModel enemySharedModel)
+        {
+            EnemySharedModel = enemySharedModel;
+        }
+    }
+}
