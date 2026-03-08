14f1b9d1d404983022ed79cc147e51eeff3cb224
Sergei Lind <sergei.lind@tutamail.com>
2026-03-08 18:57:01 +0300

    add: enemy shared model

--- Multiplayer-Roguelike.Shared/Models/GameSessionSharedModel.cs
+++ Multiplayer-Roguelike.Shared/Models/GameSessionSharedModel.cs
@@ -8,14 +8,14 @@ namespace Shared.Models
         public readonly SharedModelCollection<CharacterSharedModel> Characters =
             new SharedModelCollection<CharacterSharedModel>("characters", CharacterSharedModel.Create);
 
-        public readonly SharedModelCollection<NpcSharedModel> NPCs = new SharedModelCollection<NpcSharedModel>("npcs", NpcSharedModel.Create);
+        public readonly SharedModelCollection<EnemySharedModel> Enemies = new SharedModelCollection<EnemySharedModel>("enemies", EnemySharedModel.Create);
 
         public readonly Property<bool> IsRun = new Property<bool>("is_run", false);
 
         public GameSessionSharedModel(string id) : base(id)
         {
             Children.Add(Characters.Id, Characters);
-            Children.Add(NPCs.Id, NPCs);
+            Children.Add(Enemies.Id, Enemies);
             Children.Add(IsRun.Id, IsRun);
         }
     }
