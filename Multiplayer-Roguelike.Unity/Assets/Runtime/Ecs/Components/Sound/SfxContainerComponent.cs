d390dd9e67a0615a836a548546f00b941fd74fdb
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-03-10 00:00:49 +0300

    add: sfx container component

---
+++ Multiplayer-Roguelike.Unity/Assets/Runtime/Ecs/Components/Sound/SfxContainerComponent.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Runtime.Ecs.Components.Sound
+{
+    public class SfxContainerComponent : IComponent
+    {
+        public GameObject Container { get; }
+
+        public SfxContainerComponent(GameObject container)
+        {
+            Container = container;
+        }
+    }
+}
