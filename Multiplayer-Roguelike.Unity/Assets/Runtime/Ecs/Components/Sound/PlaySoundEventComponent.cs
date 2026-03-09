9c0f4d5757f480875beaa8e16c946a835f9e9f13
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-03-10 00:00:36 +0300

    add: play sound event component

---
+++ Multiplayer-Roguelike.Unity/Assets/Runtime/Ecs/Components/Sound/PlaySoundEventComponent.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Runtime.Ecs.Components.Sound
+{
+    public class PlaySoundEventComponent : IComponent
+    {
+        public AudioClip Clip { get; }
+
+        public PlaySoundEventComponent(AudioClip clip)
+        {
+            Clip = clip;
+        }
+    }
+}
