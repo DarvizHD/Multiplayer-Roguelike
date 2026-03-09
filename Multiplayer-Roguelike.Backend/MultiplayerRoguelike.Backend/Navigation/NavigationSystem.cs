3e96f9cdffae44b57d624d2987dd02b4d72be2e0
Sergei Lind <sergei.lind@tutamail.com>
2026-03-09 18:29:03 +0300

    add: threshold for changing enemy positions

--- Multiplayer-Roguelike.Backend/MultiplayerRoguelike.Backend/Navigation/NavigationSystem.cs
+++ Multiplayer-Roguelike.Backend/MultiplayerRoguelike.Backend/Navigation/NavigationSystem.cs
@@ -6,6 +6,9 @@ namespace Backend.Navigation
 {
     public class NavigationSystem : RegisterServerSystem<SessionModel>
     {
+        private const float _positionThreshold = 0.05f;
+        private const float _positionThresholdSqr = _positionThreshold * _positionThreshold;
+
         public NavigationSystem(string id) : base(id)
         {
         }
@@ -16,8 +19,14 @@ namespace Backend.Navigation
 
             foreach (var enemy in session.Enemies.Models.Values)
             {
-                var agent = session.Navigation.Crowd.GetAgent(enemy.CrowdAgent.idx);
-                enemy.Shared.Position.Value = new Vector3(-agent.npos.X, agent.npos.Y, agent.npos.Z);
+                var agent = enemy.CrowdAgent;
+                var newPosition = new Vector3(-agent.npos.X, agent.npos.Y, agent.npos.Z);
+                var oldPosition = enemy.Shared.Position.Value;
+
+                if ((newPosition - oldPosition).LengthSquared() > _positionThresholdSqr)
+                {
+                    enemy.Shared.Position.Value = newPosition;
+                }
             }
         }
     }
