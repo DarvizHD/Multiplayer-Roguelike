b67433c1034acfb186dcb98f1f039ea7fddeb717
Sergei Lind <sergei.lind@tutamail.com>
2026-03-11 18:59:52 +0300

    add: enemy attack system

---
+++ Multiplayer-Roguelike.Backend/MultiplayerRoguelike.Backend/Enemies/Combat/EnemyAttackSystem.cs
@@ -0,0 +1,60 @@
+﻿using Backend.ServerSystems;
+using Backend.Session;
+using Shared.Models.Player;
+
+namespace Backend.Enemies.Combat
+{
+    public class EnemyAttackSystem : IServerSystem
+    {
+        public string Id { get; }
+
+        private SessionModel SessionModel { get; }
+
+        public EnemyAttackSystem(string id, SessionModel sessionModel)
+        {
+            Id = id;
+            SessionModel = sessionModel;
+        }
+
+        public void Update(float deltaTime)
+        {
+            foreach (var enemy in SessionModel.Enemies.Models.Values)
+            {
+                UpdateEnemy(enemy, deltaTime);
+            }
+        }
+
+        private void UpdateEnemy(EnemyModel enemy, float deltaTime)
+        {
+            var attack = enemy.EnemyAttack;
+
+            if (attack.CooldownTimer > 0)
+            {
+                attack.CooldownTimer -= deltaTime;
+                return;
+            }
+
+            if (enemy.Shared.TargetPlayerId != null)
+            {
+                if (SessionModel.GameSessionSharedModel.Characters.TryGet(enemy.Shared.TargetPlayerId.Value, out var targetPlayer))
+                {
+                    var distance = (enemy.Shared.Position.Value - targetPlayer.Position.Value).LengthSquared();
+
+                    if (!(distance > attack.Range * attack.Range))
+                    {
+                        PerformAttack(enemy, targetPlayer);
+                    }
+                }
+            }
+        }
+
+        private void PerformAttack(EnemyModel enemy, CharacterSharedModel player)
+        {
+            var attack = enemy.EnemyAttack;
+
+            player.Health.Value -= attack.Damage;
+
+            attack.CooldownTimer = 0;
+        }
+    }
+}
--- Multiplayer-Roguelike.Backend/MultiplayerRoguelike.Backend/Session/SessionPresenter.cs
+++ Multiplayer-Roguelike.Backend/MultiplayerRoguelike.Backend/Session/SessionPresenter.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using Backend.Enemies;
 using Backend.Player;
+using Backend.ServerSystems;
 using Shared.Models.Player;
 
 namespace Backend.Session
@@ -9,15 +10,19 @@ namespace Backend.Session
     {
         private readonly SessionModel _model;
         private readonly WorldModel _world;
+        private readonly ServerSystemCollection _serverSystems;
 
         private readonly EnemyModelCollectionPresenter _enemyModelCollectionPresenter;
+        private readonly EnemyAttackSystem _attackSystem;
 
         public SessionPresenter(SessionModel model, WorldModel worldModel)
         {
             _model = model;
             _world = worldModel;
+            _serverSystems = worldModel.ServerSystems;
 
             _enemyModelCollectionPresenter = new EnemyModelCollectionPresenter(model.Enemies, model, worldModel.ServerSystems);
+            _attackSystem = new EnemyAttackSystem($"{_model.Id}: enemy-attack-system", _model);
         }
 
         public void Enable()
@@ -26,6 +31,7 @@ namespace Backend.Session
             _model.Players.OnRemoved += OnPlayerRemoved;
 
             _enemyModelCollectionPresenter.Enable();
+            _serverSystems.Add(_attackSystem);
         }
 
         public void Disable()
@@ -34,6 +40,8 @@ namespace Backend.Session
             _model.Players.OnRemoved -= OnPlayerRemoved;
 
             _enemyModelCollectionPresenter.Disable();
+
+            _serverSystems.Remove(_attackSystem);
 
             foreach (var player in _model.Players.Models.Values.Where(p => !p.IsActive))
             {
