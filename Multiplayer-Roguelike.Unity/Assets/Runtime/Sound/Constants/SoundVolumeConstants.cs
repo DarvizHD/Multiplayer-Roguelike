a966bff8f5a1f390e6bb826505f52b1f89b92ff0
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-03-17 16:24:40 +0300

    add: sound volume constants

---
+++ Multiplayer-Roguelike.Unity/Assets/Runtime/Sound/Constants/SoundVolumeConstants.cs
@@ -0,0 +1,18 @@
+namespace Runtime.Sound.Constants
+{
+    public static class SoundVolumeConstants
+    {
+        public static class Ambient
+        {
+            public static readonly float Menu = 1f;
+            public static readonly float Gameplay = 0.75f;
+        }
+
+        public static class Weapon
+        {
+            public static readonly float PistolShot = 0.75f;
+            public static readonly float BaseballBatHit = 0.75f;
+            public static readonly float WeaponReload = 0.75f;
+        }
+    }
+}
