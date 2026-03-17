f1bbe76e5c3eafee576f587c93a37cc64b3033d7
MiramiKoi <153764172+MiramiKoi@users.noreply.github.com>
2026-03-17 17:08:10 +0300

    chg: update SoundVolumeConstants.cs

--- Multiplayer-Roguelike.Unity/Assets/Runtime/Sound/Constants/SoundVolumeConstants.cs
+++ Multiplayer-Roguelike.Unity/Assets/Runtime/Sound/Constants/SoundVolumeConstants.cs
@@ -4,15 +4,25 @@ namespace Runtime.Sound.Constants
     {
         public static class Ambient
         {
-            public static readonly float Menu = 1f;
-            public static readonly float Gameplay = 0.75f;
+            public const float Menu = 0.7f;
+            public const float Gameplay = 0.35f;
         }
 
-        public static class Weapon
+        public static class Sfx
         {
-            public static readonly float PistolShot = 0.75f;
-            public static readonly float BaseballBatHit = 0.75f;
-            public static readonly float WeaponReload = 0.75f;
+            public const float Default = 0.75f;
+        }
+
+        public static class Voice
+        {
+            public const float Default = 0.25f;
+        }
+
+        public static class Fade
+        {
+            public const float Short = 0.25f;
+            public const float Default = 0.5f;
+            public const float Long = 1f;
         }
     }
 }
